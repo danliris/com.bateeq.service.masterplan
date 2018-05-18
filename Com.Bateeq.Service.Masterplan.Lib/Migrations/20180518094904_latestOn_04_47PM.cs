804330102e7c64323ad8194a8193309f0d3efc6f
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-05-18 17:09:22 +0700

    update for today
    check if any code has exist

---
+++ Com.Bateeq.Service.Masterplan.Lib/Migrations/20180518094904_latestOn_04_47PM.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.Bateeq.Service.Masterplan.Lib.Migrations
+{
+    public partial class latestOn_04_47PM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Commodities_Code",
+                table: "Commodities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Commodities_Code",
+                table: "Commodities",
+                column: "Code",
+                unique: true,
+                filter: "[Code] IS NOT NULL");
+        }
+    }
+}
--- Com.Bateeq.Service.Masterplan.Lib/Models/Commodity.cs
+++ Com.Bateeq.Service.Masterplan.Lib/Models/Commodity.cs
@@ -1,6 +1,7 @@
 ﻿using Com.Moonlay.Models;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Com.Bateeq.Service.Masterplan.Lib.Models
 {
@@ -11,7 +12,14 @@ namespace Com.Bateeq.Service.Masterplan.Lib.Models
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            return new List<ValidationResult>();
+
+            MasterplanDbContext dbContext = (MasterplanDbContext)validationContext.GetService(typeof(MasterplanDbContext));
+
+
+            if (dbContext.Commodities.Count(p => p._IsDeleted.Equals(false) && p.Id != this.Id && p.Code.Equals(this.Code)) > 0)
+            {
+                yield return new ValidationResult($"Commodity Code with {this.Code} is already exists", new List<string> { "Code" });
+            }
         }
     }
 }
